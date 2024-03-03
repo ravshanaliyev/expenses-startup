4b14145cbc0f7f0bf1a3d46a1e017a964b62ecd4
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-03-03 16:14:05 +0500

    modal

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,9 +1,21 @@
 import Image from 'next/image'
 import { ModeToggle } from './toggle-theme'
+import { Button } from '../ui/button'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between max-w-[1200px] mx-auto my-6 items-center'>
+        <div className='flex justify-between max-w-[1200px] mx-auto my-6 items-center px-4'>
             <Image
                 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
                 src="/next.svg"
@@ -14,7 +26,30 @@ const Navbar = () => {
             />
             <div className="flex items-center gap-4">
                 <ModeToggle />
-                <p>create</p>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button>Create</Button>
+                    </DialogTrigger>
+                    <DialogContent className="max-w-[400px] lg:max-w-[425px] mx-4 rounded-lg">
+                        <DialogHeader>
+                            <DialogTitle>Create an expense</DialogTitle>
+                            <DialogDescription>
+                                Fill in the form to create a new expense
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                            <div className=" gap-4">
+                                <Input id="name" placeholder="chingiz" className="col-span-3" />
+                            </div>
+                            <div className="gap-4">
+                                <Input type='number' id="money" placeholder="100000" className="col-span-3" />
+                            </div>
+                        </div>
+                        <DialogFooter>
+                            <Button className='w-full' type="submit">Create an expense</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
             </div>
         </div>
     )
